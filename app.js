398573969c62a522c8758562c968c22d40187a33
Avanish Gupta <avanish.gupta.official1@gmail.com>
2023-01-07 15:36:11 +0530

    Ex04: homework

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// first text area
+var txtFirst = document.querySelector("textarea");
+
+//btn with class "btn-primary"
+var btnPrimary = document.querySelector(".btn-primary");
+
+//element with id "input-btn"
+var inputButton = document.querySelector("#input-btn");
+
+//element with id "input-btn"
+var inputTranslateButton = document.querySelector("input[name = 'translator']");
 
 function clickHandler()
 {
-    outputDiv.innerText = "ajsjsjsjsjsjsj" + txtInput.value;
+    console.log("clicked");
+    console.log("main text-area"+txtInput);
 };
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler) ;
+
+btnPrimary.addEventListener("click", function primaryClickHandler()
+{
+    console.log("first text area values: "+txtFirst);
+    console.log("Inputted text : "+inputButton);
+    console.log("Inputted translator : "+inputTranslateButton);
+
+});
